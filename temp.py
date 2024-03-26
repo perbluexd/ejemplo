eab45cfb81e855b1f60cd0aa8350b722b0e74aa4
Percy <65635300+perbluexd@users.noreply.github.com>
2024-03-25 19:27:03 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,21 @@
+listaCiudades = []
+
+var = int(input("che pa cuantos paises tienes: "))
+
+for i in range(var):
+    ciudad = [
+             int(input("escribe el número de ciudad: ")),
+             str(input("escribe el nombre de la ciudad: ")),
+             int(input("escribe la población de la ciudad: "))
+           ]
+    registrado = False
+    for city in listaCiudades:
+        if (city[0] == ciudad[0]):
+            registrado = True
+            break
+    if (registrado):
+        print("ya se encuentra")
+    else: 
+        listaCiudades.append(ciudad)
+
+print(listaCiudades)
